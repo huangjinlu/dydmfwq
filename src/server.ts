ebff1d00cc94f1823f3e5a7a58037640e6c82583
huangjinlu <39671884+huangjinlu@users.noreply.github.com>
2024-07-03 11:11:53 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -19,14 +19,23 @@ router.get('/', ctx => {
             message: `lu--dyc-open-id not exist`,
         }
     }
-}).post('/api/text/antidirt', async (ctx) => {
-    console.log("get_conn_id ctx=", "ctx");
-    const res = await get_conn_id(ctx.request.header);
-    console.log(`ctx`, res);
-    ctx.body = {
-        "result": res,
-        "success": true,
-    };
+}).post('/api/start_game', async (ctx) => {
+    const conn_id = await get_conn_id(ctx.request.header);
+    if (!conn_id)
+        return 'err conn_id';
+
+    const roomId = await getRoomInfo(ctx.request.header);
+    if (!roomId)
+        return 'err roomId';
+
+    console.log('roomId=', roomId);
+
+    let startLive = await startLiveDataTaskAll(ctx.request.header);
+    if (startLive != 'ok') {
+        return 'err ' + startLive;
+    }
+
+    ctx.body = 'ok ' + conn_id;
 
 });
 
@@ -39,28 +48,85 @@ app.listen(PORT, () => {
 });
 
 async function get_conn_id(headers: any) {
-
-    // const res = await axios.post('http://ws-push.dycloud-api.service/ws/get_conn_id',
-    const res = await axios.post('https://www.huangjinlu.cn',
+    const res = await axios.post('http://ws-push.dycloud-api.service/ws/get_conn_id',
         {
             "service_id": headers['x-tt-serviceid'],
             "env_id": headers['x-tt-envid'],
             "token": headers['token']
         },
         {
-            timeout: 1000,
+            // timeout: 1000,
             headers: {
                 "Content-Type": "application/json",
-                "get_conn_idlulu": "aaabbb"
             }
         });
-
-    // const res = await axios.post('http://ws-push.dycloud-api.service/ws/get_conn_id', {
-    //     "service_id": headers['x-tt-serviceid'],
-    //     "env_id": headers['x-tt-envid'],
-    //     "token": headers['token']
-    // });    
     // data: { data: '{"conn_id":"97382664194"}', err_msg: 'success', err_no: 0 } 
-    // console.log("get_conn_id res2=", res);
-    return res.data;
+    console.log("get_conn_id 核实timeout=", res);
+    return res.data['conn_id'];
+};
+
+async function getRoomInfo(headers: any) {
+    const res = await axios.post("https://webcast.bytedance.com/api/webcastmate/info",
+        {
+            "token": headers['token']
+        },
+        {
+            // timeout: 1000,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+    if (res.data) {
+        return res.data['info']['room_id'];
+    }
+    else {
+        console.error("getRoomInfo res=", res);
+        console.error('getRoomInfo headers=', headers);
+    }
+};
+
+async function startLiveDataTaskAll(headers: any) {
+    let appId = headers['x-tt-appid'];
+    try {
+        let msgTypes = ['live_comment', 'live_like', 'live_gift'];
+        for (const msgType of msgTypes) {
+            let res = await startLiveDataTask(appId, headers, msgType);
+            if (res != 'ok')
+                return res;
+        }
+        return 'ok';
+    } catch (err) {
+        console.error('开启推送任务异常(All):', err);
+    }
+    return 'err';
+};
+
+interface Data {
+    err_no?: number;
+    err_msg?: string;
+    data?: any;
 }
+async function startLiveDataTask(appId: string, roomId: string, msgType: string) {
+    try {
+        const res = await axios.post("http://webcast.bytedance.com/api/live_data/task/start",
+            {
+                "roomid": "" + roomId,
+                "appid": appId,
+                "msg_type": msgType
+            },
+            {
+                // timeout: 1000,
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+        console.log('开启推送任务 res.data:', res.data);
+        if (res.data.err_no === 0) {
+            return 'ok'
+        }
+        console.log('开启推送任务失败:', res.data);
+    } catch (err) {
+        console.error('开启推送任务异常:', err);
+    }
+    return msgType;
+};
