aaff6cab54236b2938c17a55c66e2e31c087f657
huangjinlu <39671884+huangjinlu@users.noreply.github.com>
2024-07-03 10:17:14 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,8 @@ app.listen(PORT, () => {
 
 async function get_conn_id(headers: any) {
 
-    const res = await axios.post('http://ws-push.dycloud-api.service/ws/get_conn_id',
+    // const res = await axios.post('http://ws-push.dycloud-api.service/ws/get_conn_id',
+    const res = await axios.post('https://www.huangjinlu.cn',
         {
             "service_id": headers['x-tt-serviceid'],
             "env_id": headers['x-tt-envid'],
@@ -50,6 +51,7 @@ async function get_conn_id(headers: any) {
             timeout: 1000,
             headers: {
                 "Content-Type": "application/json",
+                "get_conn_idlulu": "aaabbb"
             }
         });
 
